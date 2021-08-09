e5f369aeec4dff1056cc1af2e876a35baacd73b4
Damerian Dark <newbmailz@gmail.com>
2021-08-09 08:43:10 -0500

    Updated some code and added a game over card.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let startBtn = document.querySelector('.startBtn');
-let wrongAnsArray = [];
+let askedQuestions = [];
 let hide = document.querySelector('.hideCard');
 let visState = hide.getAttribute("data-visibility");
 let timeEl = document.querySelector("#cdTimer");
@@ -11,6 +11,12 @@ let opt2 = document.querySelector('#opt2');
 let opt3 = document.querySelector('#opt3');
 let opt4 = document.querySelector('#opt4');
 let resp = document.querySelector('.response');
+var container = document.querySelector(".choices");
+let questionIndex = 0;
+let random;
+let questions;
+let assignedValues;
+let score = 0;
 let optVar1;
 let optVar2;
 let optVar3;
@@ -21,211 +27,197 @@ let rI3;
 let rI4;
 
 let questionsArray = [{
-question: "What color is Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",
+    question: "What color is Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
 },
 {
-question: "What color is 2Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",},
+    question: "What color is 2Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
+},
 {
-question: "What color is 3Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",},
+    question: "What color is 3Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
+},
 {
-question: "What color is 4Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",},
+    question: "What color is 4Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
+},
 {
-question: "What color is 5Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",},
+    question: "What color is 5Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
+},
 {
-question: "What color is 6Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",},
+    question: "What color is 6Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
+},
 {
-question: "What color is 7Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",},
+    question: "What color is 7Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
+},
 {
-question: "What color is 8Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",},
+    question: "What color is 8Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
+},
 {
-question: "What color is 9Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",},
+    question: "What color is 9Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
+},
 {
-question: "What color is 10Blue?",
-cAns: "It's Blue",
-iAnsOne: "It's Green",
-iAnsTwo: "It's Yellow",
-iAnsThree: "It's Orange",},
+    question: "What color is 10Blue?",
+    cAns: "It's Blue",
+    iAnsOne: "It's Green",
+    iAnsTwo: "It's Yellow",
+    iAnsThree: "It's Orange",
+},
 ]
 
-function startGame(){ 
-    if (visState === "visible"){
-       hide.dataset.visibility = "hidden";
-       hide.style.visibility ="hidden";
-    }else {
+function startGame() {
+    if (visState === "visible") {
+        hide.dataset.visibility = "hidden";
+        hide.style.visibility = "hidden";
+    } else {
         return;
     }
     startTimer();
-    askQuests();        
+    askQuests();
 
 };
 
-function startTimer(){
+function startTimer() {
 
-    let timerInterval = setInterval(function() {
+    let timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.innerHTML = `Time: ${secondsLeft} seconds`;
-            if(secondsLeft === 0) {
-          // Stops execution of action at set interval
-          clearInterval(timerInterval);
-          // Calls function to create and append image
-          gameOver();
-        }
-    
-      }, 1000);
-    }
-    
-    function askQuests (){
-/* need tocheck to see if the index was used already when it's 
-randomly choosen */   
-        let random = Math.floor(Math.random()*(questionsArray.length));
-        let questions = questionsArray[random];
-        let assignedValues = Object.values(questions)
+        if (secondsLeft <= 0) {
+            
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
 
-        let i = 0;
-        let rando; 
+            gameOver();
+        }
+
+    }, 1000);
+}
+
+function askQuests() {
+     random = Math.floor(Math.random() * (questionsArray.length));
+     while (askedQuestions.includes(random)){
+        random = Math.floor(Math.random() * (questionsArray.length)); 
+ 
+     }
+     askedQuestions.push(random);
+     questions = questionsArray[random];
+     assignedValues = Object.values(questions)
+    let rando;
+    questionIndex++;
     console.log(assignedValues);
     let newassignedValues = assignedValues.slice(1);
     console.log(newassignedValues);
-    rando = Math.floor(Math.random()*(newassignedValues.length));
+    rando = Math.floor(Math.random() * (newassignedValues.length));
     console.log(rando);
-    optVar1 = rando;    
+    optVar1 = rando;
     rI1 = newassignedValues.splice(rando, 1);
-    rando = Math.floor(Math.random()*(rI1.length));
+    rando = Math.floor(Math.random() * (rI1.length));
     console.log(rando);
-    optVar2 = rI1[rando];    
+    optVar2 = rI1[rando];
     rI2 = newassignedValues.splice(rando, 1);
-    rando = Math.floor(Math.random()*(rI2.length));
+    rando = Math.floor(Math.random() * (rI2.length));
     console.log(rando);
-    optVar3 = rI2[rando];    
-    rI3 =newassignedValues.splice(rando, 1);
-    rando = Math.floor(Math.random()*(rI3.length));
+    optVar3 = rI2[rando];
+    rI3 = newassignedValues.splice(rando, 1);
+    rando = Math.floor(Math.random() * (rI3.length));
     console.log(rando);
-    optVar4 = rI3[rando];;    
+    optVar4 = rI3[rando];;
     rI4 = newassignedValues.splice(rando, 1);
 
-
-// while (i < 5){   
-//     if (rando === rI1 || rando === rI2 || rando === rI3 || optVar4 === 0){
-//         if (rando === rI1 || rando === rI2 || optVar3 === 0){
-//             if (rando === rI1 && rI2){
-//                 if (rando = rI1 || opt2Var === 0){
-//                     optVar2 = rando;
-//                     i++;
-//                 }else{
-//                 optVar1 = rando;
-//                 i++  }
-
+    opt1.innerHTML = `1: ${rI1}`;
+    opt2.innerHTML = `2: ${rI2}`;
+    opt3.innerHTML = `3: ${rI3}`;
+    opt4.innerHTML = `4: ${rI4}`;
+    askQuest.innerHTML = questions.question;
+    
+    /*  
+    let index = el.getAttribute("data-number");
+    let state = el.getAttribute("data-state");
+  
+  
+        let index = el.getAttribute("data-number");
+        let state = el.getAttribute("data-state");
+        // el.dataset.state is the same thing
+        console.log (index);
+        console.log(el);
+      // data.state.hidden;
+      //data.state.visible;
+      
+        if (state === "hidden"){
+          el.dataset.state = "visible";
+          el.innerHTML = index;
+        } else {
+          el.dataset.state = "hidden";
+          el.innerHTML = "";
+        }
+        // TODO: Complete function
         
-//                  } else {
-//              optVar3= rando;
-//              i++
+      });  */
 
-//          }
-                        
-//          } else{
-//             optVar4= rando;
-//             i++
-            
-//         }            
-        
-//     } else{ }
-//     console.log(i);
-// }        
-        opt1.innerHTML = rI1;
-        opt2.innerHTML = rI2;
-        opt3.innerHTML = rI3;
-        opt4.innerHTML = rI4;
-        askQuest.innerHTML = questions.question;
-        resp.innerHTML = "Wrong!";
-    // if (){
-    //     rI1 = random;
-    //     Opt1Var = 1;
-    //     console.log(i);
-    //     i++;
-    // } else if (opt2Var === 0){
-    //     rI2 = rando;
-    //     Opt2Var = 1;
-    //     console.log(i);
-    //     i++;
-    // } else if (opt3Var === 0){
-    //     rI3 = rando
-    //     opt3Var = 1;
-    //     console.log(i);
-    //     i++;
-    // }else if(opt4Var === 0){ 
-    //     rI4 = rando;
-    //     opt4Var = 1;
-    //     console.log(i);
-    //     i++;
-    // } else {
-    //     console.log(i);
-    // }     
-    
- 
-//     function randomIndexes () {
-// //randomly chooses an index
-//     //     rando = Math.floor(Math.random()*(newassignedValues.length));
-//     //     if (rI1 === "undefined"){
-//     //         rI1 = rando;
-//     //     }else if (rI2 === "undefined"){
-//     //         rI2 = rando;
-//     //     } else if ((rI3 === "undefined"){
-//     //         rI3 = random;
-//     //     } else if(rI4 === "undefined"){
-//     //         rI3 = random;
-//     //     }
-       
-//     // }
-
-
-// }
-    }    
-    // let options = Math.floor(Math.random()*3);
-    // let value1 = options ++;
-    
-    
-    
-        
-
-
-function gameOver () {
 
 
 }
 
+function gameOver() {
+    let gameOver = document.querySelector('.gameOverCard');
+    gameOver.style.visibility = "visible";
+    console.log("GameOver");
+
+}
+
+container.addEventListener("click", function (event){
+    // resp.innerHTML = "Wrong!";
+    /**When a user makes a guess this wil determine if it wsa correct or not*/
+    let el = event.target.id;
+    let correctAns = questions.cAns;
+    console.log("Event:" + el);
+    if (questionsArray.length === questionIndex){
+        gameOver();
+    }
+    if (el === correctAns) {
+        console.log("Event:" + el);
+        score++;
+        resp.innerHTML = "Correct!";
+    } else {
+        resp.innerHTML = "Wrong!"
+        if (secondsLeft -5 <= 0){
+            gameOver();
+        } else{
+        secondsLeft = secondsLeft - 5;
+    }
+    }
+    askQuests();
+});
